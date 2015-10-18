f6250e50be1f9f37836a75be002c65e57e4d2942
gpiotti <elbarzata>
2015-10-18 11:40:58 -0300

    Merge branch 'master' of https://github.com/gpiotti/Streaming
    Conflicts:
    	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/ez/streaming/MainActivity.java
+++ app/src/main/java/ez/streaming/MainActivity.java
@@ -7,12 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
+
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.ToggleButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
